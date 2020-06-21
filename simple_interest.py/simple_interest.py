f777554ab416bfa5cbbc583ae42e0a25c0152279
Navee424 <67116432+Navee424@users.noreply.github.com>
2020-06-21 10:37:49 +0530

    Update simple_interest.py

--- simple_interest.py/simple_interest.py
+++ simple_interest.py/simple_interest.py
@@ -1,1 +1,8 @@
 # python-assignment-1
+p=float(input("enter the value of principle p"))
+t=float(input("enter the value of time t"))
+r=float(input("enter the value of rate of interest r"))
+s=(p*t*r)/100
+print("simple interest is",s )
+
+
