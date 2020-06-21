272a9ebfc14404db5da0397cdd6827aa0ae7c8d5
Navee424 <67116432+Navee424@users.noreply.github.com>
2020-06-21 10:40:37 +0530

    Update calpro.py

--- calpro.py
+++ calpro.py
@@ -1,1 +1,37 @@
 # python-assignment-1
+operation = input('''
+Please type what type of opration u want to do=( +,-,*,/,%,**,//)>>>''')
+
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
+
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
+
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
+
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
+
+elif operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
+    
+elif operation == '%':
+    print('{} % {} = '.format(number_1, number_2))
+    print(number_1 % number_2)
+    
+elif operation == '**':
+    print('{} ** {} = '.format(number_1, number_2))
+    print(number_1 ** number_2)
+    
+elif operation == '//':
+    print('{} // {} = '.format(number_1, number_2))
+    print(number_1 // number_2)
+
+else:
+    print('You have not typed a valid operator, please run the program again.')
